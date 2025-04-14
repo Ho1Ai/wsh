3b62e57a7fd7b52c632ddababedecd9b5c4e0941
Ho1Ai Arch <koekto33309@gmail.com>
2025-04-14 22:33:39 +0300

    data transfer

--- src/main.c
+++ src/main.c
@@ -2,14 +2,27 @@
 #include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <dirent.h>
+//#include <dirent.h>
+//#include <sys/wait.h>
 
 const char UNAME[] = "user";
 
+
+
 void run_command(const char *name){
 	char path[256];
+	//pid_t pid = fork();
 	snprintf(path, sizeof(path), "../bin/%s/%s", name, name);
-	if (access (path, F_OK) != -1)
+	if (access (path, F_OK) != -1 && access(path, R_OK) != -1){ // R_OK?
+	/*	if (pid == 0) {
+			printf("wsh: command found, do nothing\n");	
+		} else {
+			
+		}*/
+		printf("wsh: command found, do nothing\n");
+	} else {
+		printf("wsh: Command not found, unfortunately =(\n");
+	}
 }
 
 void shell_input(){
@@ -17,10 +30,15 @@ void shell_input(){
 	printf("[wsh@%s] >> ", UNAME);
 	fgets(input, sizeof(input), stdin);
 	input[strcspn(input, "\n")]=0;
+	if(strcmp(input, "")!=0){ 
 	if(strcmp(input, "exit")==0){
-		printf("Goodbye!");
-	} else {	
-		printf("You have entered: %s\n", input);
+			printf("Goodbye!");
+		} else {
+			printf("You have entered: %s\n", input);
+			run_command(input);
+			shell_input(); //recursive call of this function, which means that it is always in input status
+		}
+	} else {
 		shell_input();
 	}
 }

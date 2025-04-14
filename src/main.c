6b9aec4443a4837ac18c97c92acd5450c5ba1f68
Ho1Ai desktop <koekto33309@gmail.com>
2025-04-14 00:28:24 +0000

    data transfer

---
+++ src/main.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <dirent.h>
+
+const char UNAME[] = "user";
+
+void run_command(const char *name){
+	char path[256];
+	snprintf(path, sizeof(path), "../bin/%s/%s", name, name);
+	if (access (path))
+}
+
+void shell_input(){
+	char input[256];
+	printf("[wsh@%s] >> ", UNAME);
+	fgets(input, sizeof(input), stdin);
+	input[strcspn(input, "\n")]=0;
+	if(strcmp(input, "exit")==0){
+		printf("Goodbye!");
+	} else {	
+		printf("You have entered: %s\n", input);
+		shell_input();
+	}
+}
+
+int main() {
+	shell_input();
+	return 0;
+}

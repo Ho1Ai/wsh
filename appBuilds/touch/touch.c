ae292f489c842bd42156e738bb3e5ec93b7e6a73
Ho1Ai <161603129+Ho1Ai@users.noreply.github.com>
2025-04-26 19:09:58 +0300

    updated some docs for args list (argv[]) in touch.c

--- appBuilds/touch/touch.c
+++ appBuilds/touch/touch.c
@@ -7,8 +7,8 @@
 
 /* Args order:
  *
- * 0. executed command
- * 1. pwd (current location) from main.c (I mean wsh)
+ * 0. pwd (current location) from main.c (I mean wsh)
+ * 1. executed command
  * 2. touch arg number 1
  * etc. which is not used
  * 
